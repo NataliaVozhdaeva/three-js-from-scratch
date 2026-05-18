122d32b5b1ffa5dad67c39ff4e95cd3ff6ef2ed9
NataliaVozhdaeva <lomznat@gmail.com>
2026-05-18 23:35:35 +1200

    feat: add basic cube

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('Someone was here');
+import * as THREE from 'three';
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.append(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDampling = true;
+
+const geoCube = new THREE.BoxGeometry();
+const cubeMat = new THREE.MeshStandardMaterial({
+  color: 0x001272,
+});
+const cube = new THREE.Mesh(geoCube, cubeMat);
+scene.add(cube);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+scene.add(hemiLight);
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.02;
+  renderer.render(scene, camera);
+  controls.update();
+}
+
+animate();
