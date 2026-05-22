a3276239f21cc1d15c282e6e9f555146e1f98c92
NataliaVozhdaeva <lomznat@gmail.com>
2026-05-23 00:22:29 +1200

    feat: add fire, WIP

--- pages/fire-effect.js
+++ pages/fire-effect.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { getParticleSystem } from '../src/getParticleSystem.js';
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -15,16 +16,24 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.append(renderer.domElement);
 
-// const controls = new OrbitControls(camera, renderer.domElement);
-// controls.enableDampling = true;
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDampling = true;
 
 const geo = new THREE.SphereGeometry();
 const thingMat = new THREE.MeshStandardMaterial({
   map: diffuse,
-  // color: 0xffffff,
 });
-const thing = new THREE.Mesh(geo, thingMat);
-scene.add(thing);
+const ball = new THREE.Mesh(geo, thingMat);
+ball.position.set(0, -0.75, 0);
+scene.add(ball);
+
+const smoke = getParticleSystem({
+  camera,
+  emitter: ball,
+  parent: scene,
+  rate: 25,
+  texture: '../src/imgs/fire.png',
+});
 
 const commonLight = new THREE.HemisphereLight(0xccffff, 0x666600);
 scene.add(commonLight);
@@ -32,7 +41,10 @@ scene.add(commonLight);
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  // controls.update();
+  smoke.update(0.016);
+  ball.rotation.x += 0.01;
+  ball.rotation.y += 0.02;
+  controls.update();
 }
 
 animate();
